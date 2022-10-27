782f8791b2a0d1a62deb7a28b8e5d3c32ffaafda
hhertout <hugues.hertout@gmail.com>
2022-10-27 21:37:37 +0200

    fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,10 @@ use App\Model\ActualiteManager;
 
 class HomeController extends AbstractController
 {
-    /**
-     * Display home page
-     */
     public function index(): string
     {
         $actuManager = new ActualiteManager();
         $homeActu = $actuManager->selectHomeActu();
-        return $this->twig->render('Home/index.html.twig', ['actualities' => $homeActu]);
+        return $this->twig->render('Home/index.html.twig', ['actualit' => $homeActu]);
     }
 }
