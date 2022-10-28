9f511200d5e711e0fe91fd90d8de55051fb898a5
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-10-28 16:57:23 +0200

    New US Family

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,7 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
+    'municipalService' => ['MunicipalServiceController', 'index',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
