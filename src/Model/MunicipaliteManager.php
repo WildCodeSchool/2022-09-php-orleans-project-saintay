c6bc5a0ff7ff82e63a2044a41d9362063f6e0875
ByZirTox <xxtedyxxrenard@gmail.com>
2022-10-28 10:38:28 +0200

    review

--- src/Model/MunicipaliteManager.php
+++ src/Model/MunicipaliteManager.php
@@ -17,25 +17,4 @@ class MunicipaliteManager extends AbstractManager
 
         return $this->pdo->query($query)->fetchAll();
     }
-
-    public function insert(array $item): int
-    {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
-        $statement->bindValue('title', $item['title'], PDO::PARAM_STR);
-
-        $statement->execute();
-        return (int)$this->pdo->lastInsertId();
-    }
-
-    /**
-     * Update item in database
-     */
-    public function update(array $item): bool
-    {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item['id'], PDO::PARAM_INT);
-        $statement->bindValue('title', $item['title'], PDO::PARAM_STR);
-
-        return $statement->execute();
-    }
 }
