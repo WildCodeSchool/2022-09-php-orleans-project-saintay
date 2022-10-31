6c362767ee495e4a9a0e75aa3cb824003dc5882e
ByZirTox <xxtedyxxrenard@gmail.com>
2022-10-31 14:35:10 +0100

    review

--- src/Model/MunicipalServiceManager.php
+++ src/Model/MunicipalServiceManager.php
@@ -6,4 +6,12 @@ use PDO;
 
 class MunicipalServiceManager extends AbstractManager
 {
+    public const TABLE = "municipalService";
+    public function selectMunicipalService(int $limit): array
+    {
+        $query = "SELECT title, description, link, date, image 
+        FROM " . self::TABLE . " 
+        ORDER BY date DESC LIMIT " . $limit;
+        return $this->pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
