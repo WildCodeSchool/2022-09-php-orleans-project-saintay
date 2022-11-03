5507880b118e71714870c8abb308df6471919546
hhertout <hugues.hertout@gmail.com>
2022-11-03 12:04:00 +0100

    saving

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,7 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
+    'allActualities' => ['HomeController', 'displayAllActualities'],
     'municipalService' => ['MunicipalServiceController', 'index',],
     'contact' => ['ContactController', 'index'],
     'login' => ['LoginController', 'login'],
