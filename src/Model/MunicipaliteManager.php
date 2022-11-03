a6ffdb77064f7775d3514e25bad4b2d27bca0b7e
ByZirTox <xxtedyxxrenard@gmail.com>
2022-11-03 12:21:37 +0100

    review

--- src/Model/MunicipaliteManager.php
+++ src/Model/MunicipaliteManager.php
@@ -7,14 +7,4 @@ use PDO;
 class MunicipaliteManager extends AbstractManager
 {
     public const TABLE = 'municipalityTeam';
-
-    public function selectAll(string $orderBy = '', string $direction = 'ASC'): array
-    {
-        $query = 'SELECT firstname, lastname, role, image FROM ' . self::TABLE;
-        if ($orderBy) {
-            $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
-        }
-
-        return $this->pdo->query($query)->fetchAll();
-    }
 }
