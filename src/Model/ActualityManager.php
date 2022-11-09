7df79c4d026adcc554707865becd77a41a20a143
hhertout <hugues.hertout@gmail.com>
2022-11-09 20:25:13 +0100

    ajout de la fonction edit

--- src/Model/ActualityManager.php
+++ src/Model/ActualityManager.php
@@ -29,4 +29,33 @@ class ActualityManager extends AbstractManager
 
         $statement->execute();
     }
+    public function selectOneById(int $id): array|false
+    {
+        $query = "SELECT id, title, description, link, date, image 
+        FROM " . self::TABLE . " 
+        WHERE id=" . $id;
+
+        return $this->pdo->query($query)->fetch();
+    }
+    public function update(int $id, array $actuality)
+    {
+        $query = "UPDATE " . self::TABLE . "
+            SET 
+            title = :title, 
+            description = :description, 
+            link = :link, 
+            date = :date, 
+            image = :image
+            WHERE id= :id";
+
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('id', $id, PDO::PARAM_INT);
+        $statement->bindValue('title', $actuality['title'], PDO::PARAM_STR);
+        $statement->bindValue('description', $actuality['description'], PDO::PARAM_STR);
+        $statement->bindValue('link', $actuality['link'], PDO::PARAM_STR);
+        $statement->bindValue('date', $actuality['date'], PDO::PARAM_STR);
+        $statement->bindValue('image', $actuality['image'], PDO::PARAM_STR);
+
+        $statement->execute();
+    }
 }
