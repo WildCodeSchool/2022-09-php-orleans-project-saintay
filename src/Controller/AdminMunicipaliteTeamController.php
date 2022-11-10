632631e807a272b704d44804e61a0c7d121285b4
ByZirTox <xxtedyxxrenard@gmail.com>
2022-11-10 10:48:58 +0100

    Finish CRUD add the teamMember

--- src/Controller/AdminMunicipaliteTeamController.php
+++ src/Controller/AdminMunicipaliteTeamController.php
@@ -20,12 +20,75 @@ class AdminMunicipaliteTeamController extends AbstractController
 
     public function add(): string
     {
-        $municipaliteManager = new MunicipaliteTeamManager();
+
+
+        $errors = $municipaliteManager = [];
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $municipaliteManager = array_map('trim', $_POST);
+            $errors = $this->validate($municipaliteManager);
+            $uploadDir = 'upload/';
+            $uploadFile = " /../" . $uploadDir . basename($_FILES['avatar']['name']);
+            $municipaliteManager['avatar'] = $uploadFile;
+
+            if (empty($errors)) {
+                $municipalite = new MunicipaliteTeamManager();
+                $municipalite->insert($municipaliteManager);
+                move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
+
+                header('Location: /admin/municipalite');
+            }
+        }
+
         return $this->twig->render(
             'Municipalite/add.html.twig',
             [
-                'employees' => $municipaliteManager->selectAll('lastname'),
+                'addMunicipaliteManager' => $municipaliteManager,
+                'errors' => $errors,
             ],
         );
+    }
+
+    private function validate(array $municipaliteManager): array
+    {
+
+        $errors = [];
+
+        $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+
+        $authorizedExtensions = ['jpg', 'jpeg', 'png'];
+
+        $maxFileSize = 2000000;
+
+        $maxLenghtCaracteres = 79;
+
+
+        if (empty($municipaliteManager["firstname"])) {
+            $errors[] = "Le prenom est obligatoire";
+        }
+        if (empty($municipaliteManager["lastname"])) {
+            $errors[] = "Le nom est obligatoire";
+        }
+
+        if (empty($municipaliteManager["role"])) {
+            $errors[] = "Le rôle est obligatoire";
+        }
+
+        if (strlen($municipaliteManager["firstname"]) >= $maxLenghtCaracteres) {
+            $errors[] = "Le prenom doit faire moin de 80 caracteres";
+        }
+
+        if (strlen($municipaliteManager["lastname"]) >= $maxLenghtCaracteres) {
+            $errors[] = "Le nom doit faire moin de 80caracteres";
+        }
+
+        if ((!in_array($extension, $authorizedExtensions))) {
+            $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
+        }
+
+        if (file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize) {
+            $errors[] = "Votre fichier doit faire moins de 2M !";
+        }
+
+        return $errors;
     }
 }
