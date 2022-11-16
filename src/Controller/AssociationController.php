c1ff8d538760496accfc55863b562ae61f4b228b
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-11-16 16:09:15 +0100

    resolve issues

--- src/Controller/AssociationController.php
+++ src/Controller/AssociationController.php
@@ -7,6 +7,15 @@ use App\Model\AssociationManager;
 
 class AssociationController extends AbstractController
 {
+    public const INPUT_MAX_LENGTH = 120;
+    public const PHONE_NUMBER_LENGTH = 10;
+
+    public function index(): string
+    {
+        $associationManager = new AssociationManager();
+        $associations = $associationManager->selectByCategory();
+        return $this->twig->render('Admin/admin-association.html.twig', ['associations' => $associations]);
+    }
     public function home(): string
     {
         return $this->twig->render('Association/home-association.html.twig');
@@ -28,5 +37,68 @@ class AssociationController extends AbstractController
             'associations' => $associations,
             'categories' => $categories
         ]);
+    }
+    public function add(): string
+    {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $association = array_map('trim', $_POST);
+
+            $errors = $this->validate($association);
+
+
+            $assoCategoryId = $this->toCategorie($association['category']);
+
+            if (empty($errors)) {
+                $associationManager = new AssociationManager();
+                $associationManager->insert($association, $assoCategoryId);
+
+                header('Location: /admin/association');
+                return '';
+            }
+        }
+        return $this->twig->render('Admin/admin-add-association.html.twig', [
+            'errors' => $errors,
+            'association' => $association ?? ''
+
+        ]);
+    }
+    private function validate(array $association): array
+    {
+
+        $errors = [];
+        if (empty($association['name'])) {
+            $errors[] = 'Le title d\'association est obligatoire';
+        }
+        if (empty($association['category'])) {
+            $errors[] = 'La categorie d\'association est obligatoire';
+        }
+        if (empty($association['description'])) {
+            $errors[] = 'La description d\'association est obligatoire';
+        }
+        if (empty($association['phone_number'])) {
+            $errors[] = 'Le numero de téléphone de l\'association est obligatoire';
+        }
+        if (
+            strlen($association['phone_number']) < self::PHONE_NUMBER_LENGTH
+            || strlen($association['phone_number']) > self::PHONE_NUMBER_LENGTH
+        ) {
+            $errors[] = 'Le  numero de téléphone doit contenir '
+                . self::PHONE_NUMBER_LENGTH . ' chiffres, et sans espaces';
+        }
+        if (strlen($association['name']) > self::INPUT_MAX_LENGTH) {
+            $errors[] = 'Le titre doit faire moins de ' . self::INPUT_MAX_LENGTH . ' caractères';
+        }
+        return $errors;
+    }
+    private function toCategorie(string $categoryInput)
+    {
+        $categoryManager = new AssoCategoryManager();
+        $categories = $categoryManager->selectAll();
+        foreach ($categories as $category) {
+            if ($categoryInput === $category['name']) {
+                return $category['id'];
+            }
+        }
     }
 }
